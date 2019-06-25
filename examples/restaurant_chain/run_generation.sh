bb97a3e60f2adf81770e27239bdc3d56f49cb9b6
Michal Zasadzinski <michal.zasadzinski@cimpress.com>
2019-06-25 18:13:46 +0200

    no message

--- examples/restaurant_chain/run_generation.sh
+++ examples/restaurant_chain/run_generation.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source ../../venv/bin/activate
-python ../../main.py --json_files data1.json data2.json --output_view restaurant_chain.view --output_explore restaurant_chain.lkml --columnn_name raw_data_column --sql_table_name chains_table
+python ../../main.py --json_files data1.json data2.json --output_view restaurant_chain.view --output_explore restaurant_chain.lkml --columnn_name DATA --sql_table_name RESTAURANT_DETAILS
---
+++ examples/zambezi/run_generation.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+source ../../venv/bin/activate
+python ../../main.py --json_files data1.json --output_view luena.view --output_explore luena.lkml --columnn_name JSON --sql_table_name '"CIMPRESS"."ZAMBEZI"."LUENA"' --table_alias LUEANA_LATEST
